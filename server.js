4dbdc3403adf557b1257351085f72d7d2efc8e0c
Assadi <assadi.halifa@gmail.com>
2021-07-08 02:06:38 +0200

    make it better

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: "*",
+  origin: "https://assadi-portfolio.herokuapp.com",
   credentials: true,
   allowedHeaders: ["sessionId", "Content-Type", "Accept", "Authorization"],
   exposedHeaders: ["sessionId"],
