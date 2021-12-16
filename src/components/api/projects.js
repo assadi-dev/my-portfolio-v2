d8e595d389b6705d9fca057bd0530f4f93b8f97d
Assadi <assadi.halifa@gmail.com>
2021-12-16 10:36:27 +0100

    add project

--- src/components/api/projects.js
+++ src/components/api/projects.js
@@ -10,29 +10,6 @@ import pic9 from "../../img/Capture9.png";
 import pic10 from "../../img/Capture10.png";
 
 const projectData = [{
-        id: 1,
-        name: "My-cms-prospect",
-        categorie: "Application web",
-        description: "Cette application permet de d'enregistré et suivre ses prospections , pour ce projet j'ai utilisé le template react-black-dashboard by Creative Tim pour l'interface, afin de me concentré sur les fonctionnalité, technos utilisé pour la partie front react ,redux , back-end symfony 5 , postegreSqQL pour la base de donnée. identifiant pour tester l'application test@gmail.com, mot de passe: 1234",
-        annee: "2021",
-        picture: pic6,
-        mois: "juin",
-        github: "https://github.com/assadi-dev/my-cms-prospect",
-        link: "http://my-cms-prospect.herokuapp.com/admin/tableau-de-board/",
-    },
-
-    {
-        id: 2,
-        name: "Twitch Clone",
-        picture: pic2,
-        categorie: "Application web",
-        annee: "2020",
-        description: "Appli web réaliseé avec le framework react js cette application et un clone du célèbre site Twitch dédié au streaming, ceci à l'aide de l'api fourni par Twitch ",
-        github: "https://github.com/assadi-dev/twitch-clone-app",
-        link: "https://react-twicht-tv.netlify.app/",
-    },
-
-    {
         id: 3,
         name: "LS Paradise",
         picture: pic3,
